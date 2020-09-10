4e796208307fc521c342706fbffa69598655185a
Stefan Matei <stevro@users.noreply.github.com>
2020-09-10 11:16:05 +0300

    Update Spout.php

--- Services/Spout.php
+++ Services/Spout.php
@@ -2,41 +2,33 @@
 
 namespace Stev\SpoutBundle\Services;
 
-use Box\Spout\Reader\ReaderFactory;
 use Box\Spout\Common\Type;
-use Box\Spout\Writer\WriterFactory;
+use Box\Spout\Reader\Common\Creator\ReaderFactory;
+use Box\Spout\Reader\ReaderInterface;
+use Box\Spout\Writer\Common\Creator\WriterFactory;
+use Box\Spout\Writer\WriterInterface;
 
-/**
- * Description of Spout
- *
- * @author stefan
- */
-class Spout {
-
-    public function __construct() {
-        
-    }
-
+class Spout
+{
     /**
-     * 
+     *
      * @param string $type
-     * @return \Box\Spout\Reader\ReaderInterface
+     * @return ReaderInterface
      */
     public function initReader($type = Type::CSV) {
-        $reader = ReaderFactory::create($type);
+        $reader = ReaderFactory::createFromType($type);
 
         return $reader;
     }
 
     /**
-     * 
+     *
      * @param string $type
-     * @return \Box\Spout\Writer\WriterInterface
+     * @return WriterInterface
      */
     public function initWriter($type = Type::CSV) {
-        $writer = WriterFactory::create($type);
+        $writer = WriterFactory::createFromType($type);
 
         return $writer;
     }
-
 }
