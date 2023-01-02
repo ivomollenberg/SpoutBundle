185dc9377b1cdc9ad1e6062a3d7780c7c5726ede
Ivo Mollenberg <atmwebdesign@gmail.com>
2023-01-02 23:16:44 +0100

    TreeBuilder fix

--- Services/Spout.php
+++ Services/Spout.php
@@ -2,18 +2,26 @@
 
 namespace Stev\SpoutBundle\Services;
 
+use Box\Spout\Common\Exception\UnsupportedTypeException;
 use Box\Spout\Common\Type;
 use Box\Spout\Reader\Common\Creator\ReaderFactory;
 use Box\Spout\Reader\ReaderInterface;
 use Box\Spout\Writer\Common\Creator\WriterFactory;
 use Box\Spout\Writer\WriterInterface;
 
-class Spout
-{
+/**
+ * Description of Spout
+ *
+ * @author stefan
+ */
+class Spout {
+
     /**
-     *
      * @param string $type
+     *
      * @return ReaderInterface
+     *
+     * @throws UnsupportedTypeException
      */
     public function initReader($type = Type::CSV) {
         $reader = ReaderFactory::createFromType($type);
@@ -22,9 +30,11 @@ class Spout
     }
 
     /**
-     *
      * @param string $type
+     *
      * @return WriterInterface
+     *
+     * @throws UnsupportedTypeException
      */
     public function initWriter($type = Type::CSV) {
         $writer = WriterFactory::createFromType($type);
